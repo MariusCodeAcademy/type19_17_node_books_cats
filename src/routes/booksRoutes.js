c08d50aae281568ea4e85fb45203b7ebb9581ca1
MariusCodeAcademy <59263762+MariusCodeAcademy@users.noreply.github.com>
2023-12-28 11:50:36 +0200

    delte for ever

--- src/routes/booksRoutes.js
+++ src/routes/booksRoutes.js
@@ -28,5 +28,19 @@ booksRouter.get('/api/books/:bookId', async (req, res) => {
 });
 
 // DELETE /api/books/2 - grazina visa knygos su id 2 informacija
+booksRouter.delete('/api/books/:bookId', async (req, res) => {
+  const bookId = +req.params.bookId;
+  const sql = 'DELETE FROM books WHERE book_id=? LIMIT 1';
+  const [rows, error] = await dbQueryWithData(sql, [bookId]);
+  if (error) {
+    res.status(500).json({ error: 'Internal server error' });
+    return;
+  }
+  if (rows.affectedRows === 1) {
+    res.json({ msg: 'book deleted' });
+    return;
+  }
+  res.status(400).json(rows);
+});
 
 module.exports = booksRouter;
